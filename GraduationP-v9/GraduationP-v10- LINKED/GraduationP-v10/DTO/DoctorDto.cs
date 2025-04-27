9486a329fe57ba27e230285af691835e2cc76266
nada112003 <nrafat277@gmail.com>
2025-04-27 16:41:33 +0300

    delete

--- GraduationP-v9/GraduationP-v10- LINKED/GraduationP-v10/DTO/DoctorDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GradutionP.DTO
-{
-    public class DoctorDto
-    {
-        public string Photo { get; set; } // تعديل اسم الخاصية ليكون بنفس تنسيق باقي الكود
-        public string FullName { get; set; } = string.Empty;
-        public string Specialization { get; set; } = string.Empty;
-    }
-}
